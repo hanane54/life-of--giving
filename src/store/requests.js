945226e60b7bb01f1579f0f25bfd719705a2a6de
TAOUIL Hanane <hanane59taouil@gmail.com>
2025-07-30 17:22:39 +0100

    Update HomePate

---
+++ src/store/requests.js
@@ -0,0 +1,76 @@
+import { useState, useCallback } from 'react';
+
+export const host="http://ec2-15-188-76-171.eu-west-3.compute.amazonaws.com:9090/account-management-service";
+export const organizationshost="http://ec2-15-188-76-171.eu-west-3.compute.amazonaws.com:9090/organisation-service";
+export const donationshost="http://ec2-15-188-76-171.eu-west-3.compute.amazonaws.com:9090/donation-service";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
+
+
+
+export const useHttpImages = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? requestConfig.body: null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
--- src/store/userContext.js
+++ src/store/userContext.js
@@ -1,35 +1,82 @@
 import React, { useState } from "react";
 
 const userContext = React.createContext({
+  userId: "",
   userName: "",
   userRole: "",
+  userToken: "",
+  userImage: "",
   loggedIn: false,
-  login: (name, role) => {},
+  login: (id, name, role, token) => {},
   logout: () => {},
 });
 
 export const UserContextProvider = (props) => {
-  const [userName, setUserName] = useState("anonymous");
-  const [userRole, setUserRole] = useState("anonymous");
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState(
+    localStorage.getItem("userName")
+      ? localStorage.getItem("userName")
+      : "anonymous"
+  );
+  const [userImage, setUserImage] = useState(
+    localStorage.getItem("userImage")
+      ? localStorage.getItem("userImage")
+      : "/images/inko.png"
+  );
+  const [userRole, setUserRole] = useState(
+    localStorage.getItem("userRole")
+      ? localStorage.getItem("userRole")
+      : "anonymous"
+  );
+  const [userId, setUserId] = useState(
+    localStorage.getItem("userId") ? localStorage.getItem("userId") : ""
+  );
+  const [userToken, setUserToken] = useState(
+    localStorage.getItem("userToken") ? localStorage.getItem("userToken") : ""
+  );
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("loggedIn") ? localStorage.getItem("loggedIn") : false
+  );
 
-  const loginHandler = (user, role) => {
+  const loginHandler = (id, user, role, token, image) => {
     setUserName(user);
     setUserRole(role);
+    setUserId(id);
+    setUserToken(token);
     setLoggedIn(true);
+    setUserImage(image ? image : "/images/inko.png");
+    localStorage.setItem("userName", user);
+    localStorage.setItem("userRole", role);
+    localStorage.setItem("userId", id);
+    localStorage.setItem("userToken", token);
+    localStorage.setItem("userImage", image ? image : "/images/inko.png");
+    localStorage.setItem("loggedIn", true);
+    console.log(user, token);
   };
 
   const logoutHandler = () => {
     setUserName("anonymous");
     setUserRole("anonymous");
+    setUserId("");
+    setUserToken("");
+    setUserImage("/images/inko.png");
     setLoggedIn(false);
+    localStorage.removeItem("userName");
+    localStorage.removeItem("userRole");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("userToken");
+    localStorage.removeItem("userImage");
+    localStorage.removeItem("loggedIn");
+    console.log(userName, userToken);
   };
 
   return (
     <userContext.Provider
       value={{
+        userId,
         userName,
         userRole,
+        userToken,
+        userImage,
         loggedIn,
         login: loginHandler,
         logout: logoutHandler,
